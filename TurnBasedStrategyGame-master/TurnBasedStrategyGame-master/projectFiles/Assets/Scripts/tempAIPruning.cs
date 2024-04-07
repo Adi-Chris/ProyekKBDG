ee837d5e6f1a68d1613386f34adf91a63641f2d3
davidtius <117499864+davidtius@users.noreply.github.com>
2024-04-08 00:42:20 +0700

    temp pruning

---
+++ TurnBasedStrategyGame-master/TurnBasedStrategyGame-master/projectFiles/Assets/Scripts/tempAIPruning.cs
@@ -0,0 +1,61 @@
+﻿// Online C# Editor for free
+// Write, Edit and Run your C# code using C# Online Compiler
+
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Try programiz.pro");
+    }
+    
+    public bool isPrune(List<int[][][][]> states, int[][][][] cur_state){
+        foreach (int[][][][] s in states){
+            if (is3DArraySamePos(s, cur_state)){
+                if (isAttResSame(s, cur_state)) return true;
+            }
+        }
+        return false;
+    }
+    
+    public bool is3DArraySamePos(int[][][][] a, int[][][][] b){
+        for (int i=0;i<a.Length;i++){
+                for (int j=0;j<a[i].Length;j++){
+                    if (a[i][j][0][0]!=b[i][j][0][0]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+    }
+    
+    public bool isAttResSame(int[][][][] a, int[][][][] b){
+        for (int i=0;i<a.Length;i++){
+                for (int j=0;j<a[i].Length;j++){
+                    if (a[i][j][1].Length!=0{
+                        int x=a[i][j][1][0];
+                        int y=a[i][j][1][1];
+                        
+                        if (a[x][y][0][0]==0) {
+                            int k=b[i][j][1][0];
+                            int l=b[i][j][1][1];
+                            
+                            if (b[k][l][0][0]==0) return true;
+                        } else {
+                            return false;
+                        }
+                    }
+                }
+            }
+            
+        for (int i=0;i<b.Length;i++){
+                for (int j=0;j<b[i].Length;j++){
+                    if (b[i][j][1].Length!=0{
+                        return false;
+                    }
+                }
+            }  
+        return true;
+    }
+}
