eac366591486b88b2185f770c80a2a3009ba6a6b
MSL-Sean7 <132664758+MSL-Sean7@users.noreply.github.com>
2024-05-02 02:36:54 +0700

    Add files via upload
    tambahan state & fitness function

---
+++ TurnBasedStrategyGame-master/TurnBasedStrategyGame-master/projectFiles/Assets/Scripts/tempFitnessFunction.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempFitnessFunction : MonoBehaviour
+{
+    //Sean
+
+    public tileMapScript map;
+    public int EvaluatePosition(int player) //Team 1 atau Team 2
+    {
+        int playerUnits = 0;
+        int playerTotalHP = 0;
+        int opponentUnits = 0;
+        int opponentTotalHP = 0;
+
+        int[,] positionWeights = new int[10, 10]
+        {
+            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            { 2, 2, 0, 0, 2, 2, 0, 0, 2, 2},
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            { 2, 2, 0, 0, 2, 2, 0, 0, 2, 2},
+            { 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+            { 1, 2, 2, 2, 2, 2, 2, 2, 2, 1},
+        };
+
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; i=j++)
+            {
+                if (map.tilesOnMap[i, j].gameObject.CompareTag("Tile"))
+                {
+                    if (map.tilesOnMap[i, j].GetComponent<ClickableTileScript>().unitOnTile != null)
+                    {
+                        /*if (unit = player)
+                         {
+                            playerUnits++;
+                            playerTotalHP++;
+                            positionalAdvantage += positionWeights[i, j];
+                         }
+                          else
+                         {
+                            opponentUnits++;
+                            opponentTotalHP++;
+                            positionalAdvantage -= positionWeights[i, j];
+                         }
+                        */
+                    }
+                }
+            }
+        }
+
+        int playerScore = playerUnits * 10 + playerTotalHP;
+        int opponentScore = opponentUnits * 10 + opponentTotalHP;
+        //int positionalScore = positionalAdvantage;
+        //int unitImportanceScore = jumlah total unit berdasarkan value
+
+        return playerScore - opponentScore; // + unitImportanceScore + positionalScore
+    }
+    
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TurnBasedStrategyGame-master/TurnBasedStrategyGame-master/projectFiles/Assets/Scripts/tempState.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class tempState : MonoBehaviour
+{
+    //Sean
+
+    // Define the dimensions of your 4D array
+    int sizeX = 10; // Board X
+    int sizeY = 10; // Board Y
+    int unitOnBoard = 2; // Unit on board
+    int unitStatus = 2; // Unit status
+
+    int[][][][] my4DArray;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeArray();
+    }
+
+    void InitializeArray()
+    {
+        // Create array
+        my4DArray = new int[sizeX][][][];
+
+        // Initialize the array
+        for (int i = 0; i < sizeX; i++)
+        {
+            my4DArray[i] = new int[sizeY][][];
+
+            for (int j = 0; j < sizeY; j++)
+            {
+                my4DArray[i][j] = new int[unitOnBoard][];
+
+                for (int k = 0; k < unitOnBoard; k++)
+                {
+                    my4DArray[i][j][k] = new int[unitStatus];
+                }
+            }
+        }
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
